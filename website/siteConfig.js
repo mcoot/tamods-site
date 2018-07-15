d7bba402b4813c0ea4664b2d85e0f47624cce978
JosephSpearritt <joe.spearritt@live.com>
2018-07-15 15:58:38 +1000

    update siteconifig

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,7 +24,7 @@ const siteConfig = {
   title: 'TAMods' /* title for your website */,
   tagline: 'Unofficial modding support for Tribes: Ascend',
   url: 'http://www.tamods.org' /* your website url */,
-  baseUrl: 'http://www.tamods.org' /* base url for your project */,
+  baseUrl: 'http://www.tamods.org/' /* base url for your project */,
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
